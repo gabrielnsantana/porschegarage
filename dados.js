0ec3d0db422f8ab4df41be7f3ca1668ac28f7982
Gabriel Nunes Santana <91695797+gabrielnsantana@users.noreply.github.com>
2024-09-07 21:44:01 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,36 @@
+let dados = [
+    {
+        titulo : "Porshe Cayenne",
+        descricao : "O Porsche Cayenne é um SUV de luxo que revolucionou o mercado automotivo ao combinar o desempenho esportivo característico da marca alemã com a versatilidade e o conforto de um veículo utilitário. Com design elegante e aerodinâmico, o Cayenne oferece uma experiência de condução emocionante, aliada a um interior sofisticado e repleto de tecnologia. Disponível em diversas versões, incluindo híbridas e totalmente elétricas, o Cayenne atende aos mais exigentes consumidores que buscam um veículo que combine luxo, performance e sustentabilidade.",
+        link : "https://www.porsche.com/brazil/pt/models/cayenne/"
+    },
+
+    {
+        titulo: "Porsche 911",
+        descricao: "O Porsche 911 é um ícone do automobilismo esportivo, conhecido por seu design clássico e desempenho excepcional. Com motor traseiro e tração traseira, o 911 oferece uma experiência de condução única e emocionante. Disponível em diversas versões, o 911 é a escolha perfeita para os entusiastas de carros esportivos que buscam desempenho e tradição.",
+        link: "https://www.porsche.com/brazil/pt/models/911/"
+    },
+
+    {
+        titulo: "Porsche 718",
+        descricao: "O Porsche 718 é um esportivo de motor central que oferece um equilíbrio perfeito entre desempenho e agilidade. Com design compacto e esportivo, o 718 proporciona uma experiência de condução emocionante e envolvente. Disponível em versões Cayman (cupê) e Boxster (conversível), o 718 é a escolha ideal para quem busca um carro esportivo ágil e divertido de dirigir.",
+        link: "https://www.porsche.com/brazil/pt/models/718/"
+    },
+    {
+        titulo: "Porsche Taycan",
+        descricao: "O Porsche Taycan é o primeiro esportivo totalmente elétrico da marca. Com design futurista e desempenho excepcional, o Taycan oferece uma experiência de condução elétrica única e emocionante. Disponível em diversas versões, o Taycan combina a tradição esportiva da Porsche com a tecnologia de ponta dos veículos elétricos.",
+        link: "https://www.porsche.com/brazil/pt/models/taycan/"
+    },
+
+    {
+        titulo: "Porsche Panamera",
+        descricao: "O Porsche Panamera é um sedã esportivo de luxo que combina elegância e performance. Com design sofisticado e interior luxuoso, o Panamera oferece um alto nível de conforto e tecnologia. Disponível em diversas versões, incluindo híbridas e esportivas, o Panamera é a escolha perfeita para quem busca um sedã que alia desempenho e sofisticação.",
+        link: "https://www.porsche.com/brazil/pt/models/panamera/"
+    },
+
+    {
+        titulo: "Porsche Macan",
+        descricao: "O Porsche Macan é um SUV compacto de luxo que combina a esportividade da Porsche com a versatilidade de um SUV. Com design elegante e ágil, o Macan oferece uma experiência de condução emocionante e confortável. Disponível em diversas versões, o Macan é a escolha ideal para quem busca um SUV esportivo e sofisticado.",
+        link: "https://www.porsche.com/brazil/pt/models/macan/"
+    } 
+];
